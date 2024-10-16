2e95864ecedbc592b5b9ad4a829d33b236114fbe
chirag <61842354+chiragpalan@users.noreply.github.com>
2024-10-16 17:45:04 +0530

    Update read_database.py

--- read_database.py
+++ read_database.py
@@ -6,7 +6,7 @@ import os
 databases = [
     "https://raw.githubusercontent.com/chiragpalan/time_series_prediction_v1/main/stock_data.db",
     "https://raw.githubusercontent.com/chiragpalan/time_series_prediction_v1/main/technical_features.db",
-    "https://raw.githubusercontent.com/chiragpalan/time_series_prediction_v1/main/joined_data.db.db"
+    "https://raw.githubusercontent.com/chiragpalan/time_series_prediction_v1/main/joined_data.db"
 ]
 
 def fetch_and_store(db_url, db_name):
