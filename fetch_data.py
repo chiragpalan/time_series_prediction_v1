c6b3f577c8dcee36d0d9a2e61580dde9455f7f79
chirag <61842354+chiragpalan@users.noreply.github.com>
2024-10-21 13:44:53 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -4,7 +4,7 @@ import sqlite3
 from datetime import datetime
 
 # Database path (in the same repository)
-DATABASE_PATH = "stock_data_1.db"
+DATABASE_PATH = "stock_data.db"
 
 # Connect to the SQLite database
 conn = sqlite3.connect(DATABASE_PATH)
